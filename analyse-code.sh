ce39fe0ade414257940545de169d59a94f148be0
Volodymyr Yahello <vyahello@gmail.com>
2020-01-05 14:47:11 +0200

    Setup project

---
+++ analyse-code.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+PACKAGE="weather"
+
+
+--check-box() {
+    printf "Start ${1} analysis ...\n"
+}
+
+
+remove-pycache() {
+    ( find . -d -name __pycache__ | xargs rm -r )
+}
+
+
+check-black() {
+    --check-box "black" && ( black --check ${PACKAGE} )
+}
+
+
+check-flake() {
+    --check-box "flake" && ( flake8 ${PACKAGE} )
+}
+
+
+check-pylint() {
+    --check-box "pylint" && ( pylint $(find ./ -iname *.py) )
+}
+
+
+check-mypy() {
+    --check-box "mypy" && ( mypy --package "${PACKAGE}" )
+}
+
+
+check-pydocstyle() {
+    --check-box "pydocstyle" && ( pydocstyle --explain --count *.py ./ )
+}
+
+
+check-unittests() {
+    --check-box "unitests" && pytest
+}
+
+
+main() {
+    remove-pycache
+    check-black && check-flake && check-pylint && check-mypy && check-pydocstyle && check-unittests
+}
+
+
+main
